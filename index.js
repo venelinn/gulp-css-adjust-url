9a42551f731405da54da7cc662bb2bddc513da21
Venelin Nikolov <venelin@venelins-mbp.ad.ypg.com>
2017-11-07 11:19:56 -0500

    Fix return link bump version

--- index.js
+++ index.js
@@ -3,50 +3,51 @@ var reworkUrl = require('rework-plugin-url');
 var through = require('through2');
 
 module.exports = function(options) {
-  var prepend = options.prepend;
-  var replace = options.replace;
-  var prependRelative = options.prependRelative;
-  var append = options.append;
+    var prepend = options.prepend;
+    var replace = options.replace;
+    var prependRelative = options.prependRelative;
+    var append = options.append;
 
-  function prependUrls(css) {
-    return rework(css)
-      .use(reworkUrl(function(url) {
-        if (url.indexOf('data:') === 0) {
-          return url;
-        } else {
-          var newUrl = url;
-          if (prepend) {
-            newUrl = prepend + newUrl;
-          } else if (prependRelative && url.charAt(0) != '/') {
-            newUrl = prependRelative + newUrl;
-          }
+    function prependUrls(css) {
+        return rework(css)
+            .use(reworkUrl(function(url) {
+                if (url.indexOf('data:') === 0) {
+                    return url;
+                } else {
+                    var newUrl = url;
+                    if (prepend) {
+                        newUrl = prepend + newUrl;
+                    } else if (prependRelative && url.charAt(0) != '/') {
+                        newUrl = prependRelative + newUrl;
+                    }
 
-          if (append) {
-            if (typeof append == 'function') {
-              newUrl = append(url);
-            } else {
-              newUrl = newUrl + append;
-            }
-          }
+                    if (append) {
+                        if (typeof append == 'function') {
+                            newUrl = append(url);
+                        } else {
+                            newUrl = newUrl + append;
+                        }
+                    }
 
-          if (replace) {
-            if (typeof replace == 'function') {
-              newUrl = replace(url);
-            } else {
-              newUrl = newUrl.replace(replace[0],replace[1]);
-            }
-          }
-        }
-      }))
-      .toString();
-  };
+                    if (replace) {
+                        if (typeof replace == 'function') {
+                            newUrl = replace(url);
+                        } else {
+                            newUrl = newUrl.replace(replace[0], replace[1]);
+                        }
+                    }
 
-  return through.obj(function(file, enc, cb) {
-    var css = prependUrls(file.contents.toString());
-    file.contents = new Buffer(css);
+                    return newUrl;
+                }
+            }))
+            .toString();
+    };
 
-    this.push(file);
-    cb();
-  });
+    return through.obj(function(file, enc, cb) {
+        var css = prependUrls(file.contents.toString());
+        file.contents = new Buffer(css);
+
+        this.push(file);
+        cb();
+    });
 };
-
